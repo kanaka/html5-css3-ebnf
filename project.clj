6243d323718c0a5040c533b19dc95140ff9538ab
Joel Martin <github@martintribe.org>
2019-05-21 23:46:27 -0400

    Bump to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject html5-css3-ebnf "0.1.0-SNAPSHOT"
+(defproject html5-css3-ebnf "0.2.0"
   :description "Generate EBNF grammars for HTML5 and CSS3 using W3C spec data."
   :url "http://example.com/FIXME"
   :license {:name "Mozilla Public License version 2"
